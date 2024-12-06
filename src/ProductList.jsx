29076dadc5663a7c6dd432af9f2fab451165c770
vivianbeatrice <vivianbea.lovelace@gmail.com>
2024-12-06 16:19:44 -0500

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,36 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
+
+
+{/*Task 1, Step 5.3 */}
+import { addItem } from CartSlice.reducer;
+{/*Task 1, Step 5.3  END*/}
+
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+
+
+
+
+    {/*Task 1, Step 4 - 5 */}
+
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+         ...prevState,
+         [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+       }));
+    };
+
+    {/*Task 1, Step 4 - 5 END */}
+
+
 
     const plantsArray = [
         {
@@ -268,6 +295,31 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+            
+
+
+
+             {/*Task 1, Step 2 - 3 */}
+
+             {plantsArray.map((category, index) => (
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                     <div className="product-list">
+                          {category.plants.map((plant, plantIndex) => (
+                          <div className="product-card" key={plantIndex}>
+                         <img className="product-image" src={plant.image} alt={plant.name} />
+                         <div className="product-title">{plant.name}</div>
+                         {/*Similarly like the above plant.name show other details like description and cost*/}
+                         <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                     </div>
+                      ))}
+                 </div>
+             </div>
+             ))}
+
+               {/*Task 1, Step 2 - 3 END*/}
+             
+
 
 
         </div>
